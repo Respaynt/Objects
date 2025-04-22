4f1039a89eddf20475d9eac75653153f0f61a831
Первый комит <suzdalcev97t@gmail.com>
2025-04-22 07:24:31 +0400

    Теперь понял

--- src/Book.java
+++ src/Book.java
@@ -2,10 +2,10 @@ import java.util.Objects;
 
 public class Book {
 private String title;
-private String author;
+private Author author;
 private Long publicationYear;
 
-    public Book(String title, String author, int publicationYear) {
+    public Book(String title, Author author, int publicationYear) {
         this.title = title;
         this.author = author;
         this.publicationYear = (long) publicationYear;
@@ -22,11 +22,11 @@ private Long publicationYear;
         this.title = title;
     }
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args){
         Author authorOne = new Author("Лев" ,"Толстой");
         System.out.println("authorOne = " + authorOne);
-        Book bookOne = new Book("Война и мир"," Толстой",1886);
-        bookOne.setAuthor(authorOne.toString());
+        Book bookOne = new Book("Война и мир",authorOne,1886);
+        bookOne.setAuthor(authorOne);
         bookOne.setPublicationYear(2000L);
         System.out.println("bookOne = " + bookOne);
 
         Author secondAuthor = new Author("Льюис", " Кэрролл");
         System.out.println("secondAuthor = " + secondAuthor);
-        Book secondBook = new Book("Алиса в Стране чудес", secondAuthor.getSecondName(), 1865);
+        Book secondBook = new Book("Алиса в Стране чудес",secondAuthor, 1865);
         System.out.println("secondBook = " + secondBook);
 
 
